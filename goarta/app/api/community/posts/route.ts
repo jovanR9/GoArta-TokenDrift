be30d87e8dd2aad4d50e7a4fd2c6880cc16b9442
Throne32 <tyronecrasto7@gmail.com>
2025-08-12 16:22:16 +0000

    added community folder

--- goarta/app/api/community/posts/route.ts
+++ goarta/app/api/community/posts/route.ts
@@ -1,4 +1,8 @@
-import { handleCreateCommunityPost } from './route_handler';
+import { handleCreateCommunityPost, handleGetCommunityPosts } from './route_handler';
+
+export async function GET() {
+  return handleGetCommunityPosts();
+}
 
 export async function POST(request: Request) {
   return handleCreateCommunityPost(request);
