64f6148673630ee881fab4dd2e45f233692dfb72
Pranav Kerkar <151400162+pranavkerkar31@users.noreply.github.com>
2025-09-05 22:32:44 +0530

    user icon

---
+++ goarta/components/UserIcon.tsx
@@ -0,0 +1,11 @@
+export default function UserIcon() {
+    return (
+        <>
+            <div className="flex gap-8">
+                <div className="h-12 w-12 overflow-hidden rounded-full ring-2 ring-gray-700 dark:ring-gray-100">
+                    <img src="https://loremflickr.com/g/200/200/girl" alt="" />
+                </div>
+            </div>
+        </>
+    );
+}
